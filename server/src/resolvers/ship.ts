bc8d46a92783e9eed267643074ce00988020749b
Uday Samsani <satyasaisamsani@gmail.com>
2021-03-25 22:15:01 +0530

    Modified: Ship resolvers

--- server/src/resolvers/ship.ts
+++ server/src/resolvers/ship.ts
@@ -1,14 +1,34 @@
-import {Query, Resolver} from 'type-graphql';
+import {Arg, Int, Query, Resolver} from 'type-graphql';
 import axios from 'axios';
 
-import Ship from '../entities/Ship'
+import Ship from '../entities/Ship';
 
 @Resolver(Ship)
-class ShipResolvers{
-    @Query(()=>[Ship])
-    async ships():Promise<Ship[]>{
-        const ships = await axios.get('https://api.spacexdata.com/v3/ships');
-        return ships.data;
+class ShipResolvers {
+    @Query(() => [Ship])
+    async getShips(): Promise<Ship[]> {
+        let ships: Ship[] = await Ship.find({});
+        if (!ships) {
+            const result = await axios.get('https://api.spacexdata.com/v3/ships');
+            ships = await Ship.save(result.data);
+        }
+        return ships;
+    }
+
+    @Query(() => [Ship])
+    async getShip(
+        @Arg('shipType', () => String) shipType: string,
+        @Arg('homePort', () => String) homePort: string,
+        @Arg('weight', () => Int) weight: number,
+    ): Promise<Ship[]> {
+        const ships: Ship[] = await Ship.find({
+            where: {
+                ship_type: shipType,
+                home_port: homePort,
+                weight_kg: weight
+            }
+        }) || [];
+        return ships;
     }
 }
 
