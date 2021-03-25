8289596ba64a345bd6b56fddc18871c8dcc95cec
Uday Samsani <satyasaisamsani@gmail.com>
2021-03-25 20:10:24 +0530

    Added: Scheduler

---
+++ server/src/utils/scheduler.ts
@@ -0,0 +1,27 @@
+import schedule from 'node-schedule';
+import axios from 'axios';
+
+import Ship from '../entities/Ship';
+
+async function staleShips() {
+    try {
+        await Ship.delete({});
+    } catch (err) {
+        console.log(err.message);
+    }
+}
+
+async function addShips(ships: [Ship]) {
+    await Ship.save(ships)
+}
+
+const dailyStale = () => {
+    schedule.scheduleJob('*/1 * * * *', async () => {
+        console.log('Scheduler: Staling ...');
+        await staleShips();
+        const result = await axios.get('https://api.spacexdata.com/v3/ships');
+        await addShips(result.data);
+        console.log('Scheduler: Staling complete');
+    });
+};
+export default dailyStale;
