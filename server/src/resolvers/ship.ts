f886447fd864e8b62f819ae47c7c4626bc61eebf
Uday Samsani <satyasaisamsani@gmail.com>
2021-03-26 12:05:31 +0530

    Modified: Query args for getShips

--- server/src/resolvers/ship.ts
+++ server/src/resolvers/ship.ts
@@ -1,12 +1,23 @@
-import {Arg, Int, Query, Resolver} from 'type-graphql';
+import {Arg, Field, Float, InputType, Int, Query, Resolver} from 'type-graphql';
 
 import Ship from '../entities/Ship';
 import {getConnection} from 'typeorm';
+
+@InputType()
+class filterOptions{
+    @Field(()=>String,{nullable:true})
+    shipType!:string
+    @Field(()=>String,{nullable:true})
+    homePort!:string
+    @Field(()=>String,{nullable:true})
+    weight!:string
+}
 
 @Resolver(Ship)
 class ShipResolvers {
     @Query(() => [Ship])
     async getShips(
+        @Arg('filterOptions', () => filterOptions,{nullable:true}) filterOptions: filterOptions,
         @Arg('limit', () => Int) limit: number,
         @Arg('cursor', () => Int, {nullable: true}) cursor: number
     ): Promise<Ship[]> {
@@ -14,15 +25,23 @@ class ShipResolvers {
             .getRepository(Ship)
             .createQueryBuilder('s')
             .orderBy('year_built', 'DESC')
-            .take(limit);
+            .take(limit)
+        if(filterOptions.shipType && filterOptions.homePort && filterOptions.weight){
+            qb.where({
+                ship_type: filterOptions.shipType,
+                home_port: filterOptions.homePort,
+                weight_kg: parseInt(filterOptions.weight)
+            })
+        }
         if (cursor) {
             qb.where('year_built < :cursor', {cursor});
         }
         return qb.getMany();
     }
 
+    /*
     @Query(() => [Ship])
-    async getShip(
+    async getShipssad(
         @Arg('shipType', () => String) shipType: string,
         @Arg('homePort', () => String) homePort: string,
         @Arg('weight', () => Int) weight: number,
@@ -44,6 +63,7 @@ class ShipResolvers {
         }
         return qb.getMany();
     }
+    */
 }
 
 export default ShipResolvers;
